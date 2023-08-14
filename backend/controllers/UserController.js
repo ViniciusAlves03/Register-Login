0c4cc82c1fccc503b57736108ba8751e7ed0678e
Vinícius Alves <vinicius.alves0503@gmail.com>
2023-08-14 20:52:19 -0300

    Adicionando Frontend

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -74,8 +74,6 @@ module.exports = class UserController {
     static async checkUser(req, res) {
         let currentUser
 
-        console.log(req.headers.authorization)
-
         if (req.headers.authorization) {
           const token = getToken(req)
           const decoded = jwt.verify(token, 'usersecret')
